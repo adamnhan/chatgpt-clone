a0730b1075474a80fb5949589bb81aa7e9da64a2
Adam Nhan <atomnano47@gmail.com>
2024-07-31 16:12:32 -0400

    added chatpage

---
+++ src/routes/chatPage/chatPage.jsx
@@ -0,0 +1,27 @@
+import { useRef } from 'react';
+import './chatPage.css'
+import { useEffect } from 'react';
+
+const ChatPage = () => {
+
+    const endRef = useRef(null)
+
+    useEffect(() => {
+        endRef.current.scrollIntoView({ behavior: "smooth" })
+    })
+    return (
+        <div className="chatPage">
+            <div className="wrapper">
+                <div className="chat">
+                    <div className='message'>Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message">Test message from ai</div>
+                    <div className="message user">Test message from user</div>
+                    <div ref={endRef}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ChatPage;
